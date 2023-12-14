f0cf940c76fe6f238acba204c4a617cdedd49b90
J.Daniel <daniel.cabezas1899@gmail.com>
2023-12-14 14:06:00 +0100

    90% test completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,15 @@
 import com.bst.BST;
 import com.exceptions.DepthException;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DepthException {
         BST bst= new BST();
-        try {
-            bst.insert(23,true);
-            bst.insert(33,true);
-        } catch (DepthException e) {
-            e.printStackTrace();
-        }
+        ArrayList list= new ArrayList();
+        BST subtree = bst.getSubTree(bst.search(0));
         System.out.println(bst.toList());
+        System.out.println(subtree.toList());
 
     }
 }
--- src/test/java/Pruebas.java
+++ src/test/java/Pruebas.java
@@ -24,14 +24,14 @@ public class Pruebas {
     @ParameterizedTest
     @ValueSource(ints = {-2500,-2499,344,2499,2500, -2501,2501})
     void insertLimitValuesRange(int value) throws DepthException {
-        if(value>2500 || value<-2500){
+        if(value>=2500 || value<-2500){
             assertThrows(Exception.class,() ->{
                 tree.insert(value,true);
             });
         }
         else{
             tree.insert(value,true);
-            assertTrue(value >= -2500 && value <= 2500);
+            assertTrue(value >= -2500 && value < 2500);
         }
     }
     @Test
@@ -48,33 +48,102 @@ public class Pruebas {
     @Test
     void searchLimitValuesDepth() throws DepthException {
         /*BUSCAMOS QUE LA PROFUNDIDA NO SEA MENOR QUE 50*/
-        assertThrows(Exception.class,() ->{
-            for (int i = 0; i < 50; i++) {
-                tree.insert(i,true);
+        assertThrows(Exception.class,()->{
+            for(int i=0;i<51;i++){
+                tree.insert(i,false);
             }
-            tree.insert(51,true);
-
-        });
-        Node resul = tree.search(51);
+            tree.insert(51,false);
+        } );
+        Node resul = tree.search(50);
         assertNull(resul);
     }
     @ParameterizedTest
     @ValueSource(ints = {-2500,-2499,344,2499,2500,-2501,2501})
     void searchLimitValuesRange(int value) throws DepthException {
-        if(value>2500 || value<-2500){
+        if(value>=2500 || value<-2500){
             assertThrows(Exception.class,() ->{
                 tree.insert(value,true);
             });
         }
         else{
             tree.insert(value,true);
-            assertTrue(value >= -2500 && value <= 2500);
+            assertTrue(value >= -2500 && value < 2500);
         }
         Node case1 = tree.search(2501);
         assertNull(case1);
         Node case2 = tree.search(-2501);
         assertNull(case2);
     }
+
+    /*-------------------CLASES DE EQUIVALENCIA--------------------------*/
+
+    /*COMPROBAR QUE EL NODO ESTÁ EN EL ARBOL*/
+    @Test
+    void searchEqClass() throws DepthException {
+        tree.insert(34,true);
+        Node notexist= tree.search(45);
+        Node exist= tree.search(34);
+        assertNull(notexist);
+        assertNotNull(exist);
+    }
+
+    /*Comprobamos que */
+    @Test
+    void depthEqClass() throws DepthException {
+        assertTrue( tree.depth()==0);
+
+        tree.insert(0,true);
+        assertTrue( tree.depth()==1);
+
+        /*Comprobamos que como maximo depth devuelve 50*/
+        assertThrows(Exception.class, () ->{
+            for (int i = 1; i <50 ; i++) {
+                tree.insert(i,true);
+            }
+            tree.insert(50,true);
+            tree.insert(51,true);
+            tree.insert(52,true);
+        });
+        assertTrue(tree.depth()<=50);
+    }
+
+    @Test
+    void toListEqClass() throws DepthException {
+        ArrayList list= new ArrayList();
+        assertTrue(tree.toList().equals(list));
+
+        for (int i = 0; i < 12 ; i++) {
+            tree.insert(i,true);
+            list.add(i);
+        }
+        assertTrue(tree.toList().equals(list));
+
+        list.add(77);
+        assertFalse(tree.toList().equals(list));
+    }
+
+    @Test
+    void getSubtreeEqClass() throws DepthException {
+        BST testTree = new BST<>();
+        for (int i = 0; i < 40; i++) {
+            tree.insert(i,true);
+        }
+        BST subTree =tree.getSubTree(tree.search(20));
+        for (int i = 20; i <40 ; i++) {
+            testTree.insert(i,true);
+        }
+        assertTrue(testTree.toList().equals(subTree.toList()));
+        /*Comprobamos que lanza una excepcion si en nodo no existe*/
+        assertThrows(Exception.class, ()->{
+            BST subTree2 = tree.getSubTree(tree.search(42));
+
+        });
+
+    }
+
+
+
+
 
 
 
